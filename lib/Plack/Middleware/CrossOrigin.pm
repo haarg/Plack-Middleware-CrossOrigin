fc7f8e07f6704e95c43a1f7e86c54cb309b21080
Graham Knop <haarg@haarg.org>
2010-09-19 07:36:44 -0500

    initial code for module

--- lib/Plack/Middleware/CrossOrigin.pm
+++ lib/Plack/Middleware/CrossOrigin.pm
@@ -1,12 +1,166 @@
 use strict;
 use warnings;
 package Plack::Middleware::CrossOrigin;
-# ABSTRACT: New module Plack::Middleware::CrossOrigin
+# ABSTRACT: Adds headers to allow Cross-Origin Resource Sharing
+use parent qw(Plack::Middleware);
+
+use Plack::Util;
+use Plack::Util::Accessor qw(
+    origins
+    headers
+    methods
+    max_age
+    expose_headers
+    credentials
+);
+
+sub call {
+    my ($self, $env) = @_;
+
+    if ($env->{HTTP_ORIGIN}) {
+        my @headers = $self->cors_headers($env);
+
+        my $res;
+        if ($env->{REQUEST_METHOD} eq 'OPTIONS') {
+            $res = [200, [ 'Content-Type' => 'text/plain' ], [] ];
+        }
+        else {
+            $res = $self->app->($env);
+        }
+        return $self->response_cb($res, sub {
+            my $res = shift;
+            push @{$res->[1]}, @headers;
+        });
+    }
+    return $self->app->($env);
+}
+
+sub cors_headers {
+    my $self = shift;
+    my $env = shift;
+    my $preflight = $env->{REQUEST_METHOD} eq 'OPTIONS';
+    my @headers;
+
+    my @allowed_origins = ref $self->origins ? @{ $self->origins } : $self->origins || ();
+    my %allowed_origins = map { $_ => 1 } @allowed_origins;
+    my @allowed_methods = ref $self->methods ? @{ $self->methods } : $self->methods || ();
+    my %allowed_methods = map { $_ => 1 } @allowed_methods;
+    my @allowed_headers = ref $self->headers ? @{ $self->headers } : $self->headers || ();
+    my %allowed_headers = map { lc $_ => 1 } @allowed_methods;
+
+    my $request_method = $env->{HTTP_ACCESS_CONTROL_REQUEST_METHOD};
+    my $request_headers = $env->{HTTP_ACCESS_CONTROL_REQUEST_HEADERS};
+    my @request_headers = $request_headers ? (split /,\s*/, $request_headers) : ();
+
+    if (! $allowed_origins{'*'} ) {
+        for my $origin (split / /, $env->{HTTP_ORIGIN}) {
+            return
+                unless $allowed_origins{$origin};
+        }
+    }
+    if ($preflight) {
+        return
+            if ! $request_method;
+        unless ( $allowed_methods{'*'} || $allowed_methods{$request_method} ) {
+            return;
+        }
+        if (! $allowed_headers{'*'} ) {
+            for my $header (@request_headers) {
+                return
+                    unless $allowed_headers{lc $header};
+            }
+        }
+    }
+    if ($self->credentials) {
+        push @headers, 'Access-Control-Allow-Origin' => $env->{HTTP_ORIGIN};
+        push @headers, 'Access-Control-Allow-Credentials' => 'true';
+    }
+    else {
+        if ($allowed_origins{'*'}) {
+            push @headers, 'Access-Control-Allow-Origin' => '*';
+        }
+        else {
+            push @headers, 'Access-Control-Allow-Origin' => $env->{HTTP_ORIGIN};
+        }
+    }
+    if ($preflight) {
+        if ($self->max_age) {
+            push @headers, 'Access-Control-Max-Age' => $self->max_age;
+        }
+        if ($allowed_methods{'*'}) {
+            push @headers, 'Access-Control-Allow-Methods' => $request_method;
+        }
+        else {
+            push @headers, 'Access-Control-Allow-Methods' => $_
+                for @allowed_methods;
+        }
+        if ( $allowed_headers{'*'} ) {
+            push @headers, 'Access-Control-Allow-Headers' => $_
+                for @request_headers;
+        }
+        else {
+            push @headers, 'Access-Control-Allow-Headers' => $_
+                for @allowed_headers;
+        }
+    }
+    if ($self->expose_headers) {
+        my @expose_headers = ref $self->expose_headers ? @{ $self->expose_headers } : $self->expose_headers;
+        push @headers, 'Access-Control-Allow-Origin' => $_
+            for @expose_headers;
+    }
+    return @headers;
+}
 
 1;
 
 =head1 SYNOPSIS
 
+    builder {
+        enable 'CrossOrigin',
+            origins => '*', methods => ['GET', 'POST'], max_age => 60*60*24*30;
+        $app;
+    };
+
 =head1 DESCRIPTION
 
+Adds Cross Origin Request Sharing headers used by recent browsers
+to allow XMLHttpRequests across domains.
+
+=head1 CONFIGURATION
+
+=over 8
+
+=item origins
+
+A list of allowed origins.  * can be specified to allow access from
+any origin.
+
+=item headers
+
+A list of allowed headers.  * can be specified to allow any headers.
+
+=item methods
+
+A list of allowed methods.  * can be specified to allow any methods.
+
+=item max_age
+
+The max length in seconds to cache the response data for.
+
+=item expose_headers
+
+A list of allowed headers to expose to the client.
+
+=item credentials
+
+Whether the resource supports credentials.
+
+=back
+
+=head1 SEE ALSO
+
+=for :list
+* L<http://www.w3.org/TR/cors/>
+
+=cut
 
--- t/basic.t
+++ t/basic.t
@@ -3,7 +3,36 @@ use warnings;
 use Test::More;
 
 use Plack::Middleware::CrossOrigin;
+use Plack::Test;
+use Plack::Builder;
 
-fail "Tests written";
+test_psgi
+    app => builder {
+        enable 'CrossOrigin',
+            origins => '*', methods => ['GET', 'POST'], max_age => 60*60*24*30;
+        sub { [ 200, [ 'Content-Type' => 'text/plain' ], [ 'Hello World' ] ] };
+    },
+    client => sub {
+        my $cb = shift;
+        my $req;
+        my $res;
+
+        $req = HTTP::Request->new(GET => 'http://localhost/');
+        $res = $cb->($req);
+        is $res->header('Access-Control-Allow-Origin'), undef, 'No extra headers added with no Origin header';
+
+        $req = HTTP::Request->new(GET => 'http://localhost/', [
+            'Origin' => 'www.example.com',
+        ]);
+        $res = $cb->($req);
+        is $res->header('Access-Control-Allow-Origin'), '*', 'Access-Control-Allow-Origin header added';
+
+        $req = HTTP::Request->new(OPTIONS => 'http://localhost/', [
+            'Access-Control-Request-Method' => 'POST',
+            'Origin' => 'www.example.com',
+        ]);
+        $res = $cb->($req);
+        is $res->header('Access-Control-Allow-Origin'), '*', 'Access-Control-Allow-Origin header added';
+    };
 
 done_testing;
